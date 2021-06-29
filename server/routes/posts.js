5b68c446ae537b5fc3b43fd9be293f5b84f98aad
sanket-sanket <sanki29281@gmail.com>
2021-06-29 22:13:33 +0530

    Initial commit

---
+++ server/routes/posts.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import { getPosts,createPost,updatePost,deletePost ,likePost} from '../controllers/posts.js';
+//createPost, likePost, deletePost } 
+
+const router = express.Router();
+
+router.get('/', getPosts);
+router.post('/', createPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
+export default router;
+
+
+
